5989e22dc4e4d7c71bfab72a03b5c76b7cbf712d
dan <elastic7327@gmail.com>
2018-03-07 10:44:39 +0900

    dont with JWT

--- src/flaskr.py
+++ src/flaskr.py
@@ -3,9 +3,14 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from flask_jwt import JWT, jwt_required, current_identity
+
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:MyNewPass@localhost/local_test'
+
+# 시크릿키를 입력하지 않으면 JWT 오류발생
+app.config['SECRET_KEY'] = 'super-secret'
 
 # flask-sqlalchemy + flask-migrate
 db = SQLAlchemy(app)
@@ -17,12 +22,37 @@ migrate = Migrate(app, db)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(500), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     # mobile = db.Column(db.String(120), unique=True, nullable=False)
-    address = db.Column(db.String(220), unique=True, nullable=False)
+    address = db.Column(db.String(220), nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    # def __repr__(self):
+        # return '<User %r>' % self.username
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+def authenticate(username, password):
+    user = User.query.filter(
+            User.username==username,
+            User.password==password).one()
+
+    return user
+
+def identity(payload):
+    user_id = payload['identity']
+    user = session.query(User).get(user_id)
+    return user.id
+
+jwt = JWT(app, authenticate, identity)
+
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def hello_jwt():
+    return jsonify({
+        'message': 'hello world!'
+    })
 
 @app.route('/', methods=['GET'])
 def hello_world():
--- src/tests/test_views.py
+++ src/tests/test_views.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import json
 
 from src.tests.base import BaseTestCase
+from src.flaskr import User
+
+from mixer.backend.flask import mixer
 
 
 class TestSimpleView(BaseTestCase):
@@ -7,3 +13,33 @@ class TestSimpleView(BaseTestCase):
     def test_hello_world(self):
         res =self.client.get("/")
         assert res.status_code == 200
+
+    def test_jwt_and_auth_test(self):
+        # 토큰없이 시도를 할때에는 401 에러는 받습니다.
+        res = self.client.get(
+                "/protected",
+                content_type="application/x-www-form-urlencoded")
+        assert res.status_code == 401
+
+        # Fake User을 만들어서, 해당 유저의 토큰을 발급받습니다.
+        u = mixer.blend(User)
+        data = json.dumps({"username": u.username, "password": u.password})
+        res = self.client.post(
+                    "/auth",
+                    data=data,
+                    content_type="application/json"
+                )
+
+        # 정상적인경우 status 200을 받습니다.
+        assert res.status_code == 200
+        assert 'access_token' in str(res.data)
+
+        # 이제 헤더에 토큰을 추가해서 다시 시도합니다.
+        token = json.loads(res.data)["access_token"]
+        res = self.client.get(
+                "/",
+                headers={'Authorization': 'JWT ' + token})
+        assert res.status_code == 200
+
+
+
