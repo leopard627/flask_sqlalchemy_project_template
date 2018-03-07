377b26478214a375afe356324c545953382fadf9
dan <elastic7327@gmail.com>
2018-03-07 12:36:51 +0900

    delete User

--- src/models.py
+++ src/models.py
@@ -3,7 +3,7 @@ from src.flaskr import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-     password = db.Column(db.String(500), nullable=False)
+    password = db.Column(db.String(500), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     # mobile = db.Column(db.String(120), unique=True, nullable=False)
     address = db.Column(db.String(220), unique=True, nullable=False)
@@ -13,3 +13,6 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
